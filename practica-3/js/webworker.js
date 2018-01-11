07010263e0665bf594493c35d86b3ad850bddec9
José Iván Martín Puertas <joseivan.martin@gmail.com>
2018-01-11 20:37:57 +0100

    Separados los JS
    Separados los javascript para que no interfieran en el resto de paginas

--- practica-3/js/webworker.js
+++ practica-3/js/webworker.js
@@ -1,8 +1,17 @@
-onmessage = function (oEvent) {
-  sum = 0;
-  for (i = 0; i < oEvent.data; i++){
-  	sum = sum + i;
-  }
+// Funcion para ejecutar el bucle de la suma usando webworker
+function webworker_init(){
+  var worker = new Worker("./js/webworker_bucle.js");
 
-  postMessage(sum);
-};
+  // Mensaje que recibimos del worker
+  worker.onmessage = function(event) {
+    document.getElementById("result").value = event.data;
+  };
+
+  // En caso de producirse un error en el worker mostramos un alert con el error
+  worker.onerror = function(error) {
+    alert("Se ha producido un error: " + error.message + "\n");
+    throw error;
+  };
+
+  worker.postMessage(document.getElementById("value").value);
+}
